0c596fb172fa76f074e87bdc88db6182e07fced3
RainyXeon <rainyxeon@gmail.com>
2025-11-11 12:18:41 +0700

    FROMGIT: build: update build script and bump 2.4.0

--- build.sh
+++ build.sh
@@ -4,9 +4,7 @@
 #
 
 SECONDS=0 # builtin bash timer
-
 DEVICE="ruby"
-
 ZIPNAME="MoonWake-Private-$(date '+%Y%m%d-%H%M').zip"
 
 export ARCH=arm64
@@ -16,19 +14,24 @@ export PATH="/root/clang-r530567/bin/:$PATH"
 
 if [[ $1 = "-c" || $1 = "--clean" ]]; then
 	rm -rf out
-	echo "Cleaned output folder"
+	echo "[MoonWake Private Build Script] Cleaned output folder"
 fi
 
-echo -e "\nStarting compilation for $DEVICE...\n"
-make -j$(nproc) \
-    O=out KCFLAGS="-O2 -march=armv8.2-a+crypto+fp16+dotprod -mcpu=cortex-a78 -mtune=cortex-a78" \
-    ARCH=arm64 \
-    LLVM=1 \
-    LLVM_IAS=1 \
-    CROSS_COMPILE=aarch64-linux-gnu- \
-    CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-    moonwake_defconfig
+echo "[MoonWake Private Build Script] Starting compilation for $DEVICE..."
 
+if [[ $1 = "-mc" || $1 = "--makeconfig" || $2 = "-mc" || $2 = "--makeconfig" ]]; then
+    echo "[MoonWake Private Build Script] Make config for $DEVICE"
+    make -j$(nproc) \
+        O=out KCFLAGS="-O2 -march=armv8.2-a+crypto+fp16+dotprod -mcpu=cortex-a78 -mtune=cortex-a78" \
+        ARCH=arm64 \
+        LLVM=1 \
+        LLVM_IAS=1 \
+        CROSS_COMPILE=aarch64-linux-gnu- \
+        CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+        moonwake_defconfig
+fi
+
+echo "[MoonWake Private Build Script] Starting building Image.gz-dtb for $DEVICE..."
 make -j$(nproc) \
     O=out KCFLAGS="-O2 -march=armv8.2-a+crypto+fp16+dotprod -mcpu=cortex-a78 -mtune=cortex-a78" \
     ARCH=arm64 \
@@ -41,17 +44,17 @@ make -j$(nproc) \
 kernel="out/arch/arm64/boot/Image.gz-dtb"
 
 if [ ! -f "$kernel" ]; then
-	echo -e "\nCompilation failed!"
+	echo "[MoonWake Private Build Script] Compilation failed!"
 	exit 1
 fi
 
-echo -e "\nKernel compiled successfully! Zipping up...\n"
+echo "[MoonWake Private Build Script] Kernel compiled successfully! Zipping up..."
 
 if [ -d "$AK3_DIR" ]; then
 	cp -r $AK3_DIR AnyKernel3
 else
-	if ! git clone -q https://github.com/kernel-build-from-rainyland/AnyKernel3 -b ruby AnyKernel3; then
-		echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
+	if ! git clone -q https://github.com/kernel-build-from-rainyland/AnyKernel3 -b ruby_private AnyKernel3; then
+		echo "[MoonWake Private Build Script] AnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
 		exit 1
 	fi
 fi
@@ -61,12 +64,5 @@ cd AnyKernel3
 zip -r9 "../$ZIPNAME" * -x .git
 cd ..
 rm -rf AnyKernel3
-echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-echo "Zip: $ZIPNAME"
-
-if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
-   head=$(git rev-parse --verify HEAD 2>/dev/null); then
-	HASH="$(echo $head | cut -c1-8)"
-fi
-
-telegram -f $ZIPNAME -M "Completed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) ! Latest commit: $HASH"
+echo "[MoonWake Private Build Script] Completed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+echo "[MoonWake Private Build Script] Zip: $ZIPNAME"
